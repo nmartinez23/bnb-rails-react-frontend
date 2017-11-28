8002164ba4d101c5e891bb11b75083da4462e539
Nick Martinez <23nicholasmartinez@gmail.com>
2017-11-28 15:25:53 -0800

    Add components dir and Map component

---
+++ src/components/Map.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class Map extends Component {
+  render() {
+    return (
+      <div>
+        Map
+      </div>
+    );
+  }
+}
+
+export default Map
